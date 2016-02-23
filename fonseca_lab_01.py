8f3ddd7856e4d84fbf595f6f9c25ff58461a4cd5
jfonseca4 <jfonseca4@my.uri.edu>
2016-02-22 22:16:28 -0500

    Added files via upload

---
+++ fonseca_lab_01.py
@@ -0,0 +1,41 @@
+#Fonseca lab_01
+
+#project 1
+
+levels = int(input("How many levels is your pyramid? ")) #ask the user how many levels is your pyramid
+
+for k in range(1,levels + 1): #pyramid starts at 1, and ends at whatever number you entered for levels + 1.
+    print("*" * k) #printing out half pyramid 
+
+
+#project 2
+
+
+levels = int(input("How many levels is your pyramid? ")) #ask the user how many levels is your pyramid
+
+for k in range(0, levels): #starts at 0 to make the odd function true
+    j = 2 * k + 1 #formula for an odd number
+    print("*" * j) #prints out pyramid in odd increments only
+#I could not figure out how to print the full pryamid unfortunately
+
+
+#project 3
+
+
+levels = int(input("How many levels is your pyramid? ")) #ask the user how many levels is your pyramid
+
+for k in range(0, levels): #starts at 0 to make the odd function true
+    j = 2 * k + 1 #formula for an odd number
+    print("atttt" * j) #prints out pyramid in odd increments only
+#makes a triangle with any thing you enter for a building block
+
+
+#project 4
+
+ #I was not able to figure out how to rotate, or even create a code for a parabola.
+#I could not figure out what to put in as a code.
+
+#project 5
+
+#I was not able to figure out this problem.
+#I could not figure out how to start this problem or what I should be using.
